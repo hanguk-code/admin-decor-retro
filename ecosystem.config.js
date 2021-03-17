871bb3e82973eea921ee5c5e43713578251204db
Orlovskyi Yuri <angelorlov@gmail.com>
2021-03-17 22:57:11 +0200

    Disable laravel server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,16 +12,16 @@ module.exports = {
                 NODE_ENV: 'production'
             }
         },
-        {
-            name: 'laravel-decor-admin',
-            script: 'php artisan serve --host=192.168.0.107 --port=8041',
-            watch: true,
-            env: {
-                NODE_ENV: 'development',
-            },
-            env_production: {
-                NODE_ENV: 'production',
-            }
-        },
+        // {
+        //     name: 'laravel-decor-admin',
+        //     script: 'php artisan serve --host=192.168.0.107 --port=8041',
+        //     watch: true,
+        //     env: {
+        //         NODE_ENV: 'development',
+        //     },
+        //     env_production: {
+        //         NODE_ENV: 'production',
+        //     }
+        // },
     ]
 };
