e67aede77a254bfd052b69a6045be0846304ff49
Orlovskyi Yuri <angelorlov@gmail.com>
2021-04-05 22:44:08 +0300

    Fix: header admin

--- frontend/components/Layout/Header.vue
+++ frontend/components/Layout/Header.vue
@@ -4,11 +4,11 @@
 
         <!-- begin:: Header Menu -->
         <button class="kt-header-menu-wrapper-close" id="kt_header_menu_mobile_close_btn"><i class="la la-close"></i></button>
-<!--        <div class="kt-header-menu-wrapper" id="kt_header_menu_wrapper">-->
-<!--            <div id="kt_header_menu" class="kt-header-menu kt-header-menu-mobile  kt-header-menu&#45;&#45;layout- ">-->
-<!--                -->
-<!--            </div>-->
-<!--        </div>-->
+        <div class="kt-header-menu-wrapper" id="kt_header_menu_wrapper">
+            <div id="kt_header_menu" class="kt-header-menu kt-header-menu-mobile  kt-header-menu--layout- ">
+
+            </div>
+        </div>
         <!-- end:: Header Menu -->
 
         <!-- begin:: Header Topbar -->
